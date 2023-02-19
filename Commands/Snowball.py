e014cd6ee98c3917142f65d7257ee8b924ab97df
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-19 19:52:33 +0100

    feat: snowball command

---
+++ Commands/Snowball.py
@@ -0,0 +1,15 @@
+import random
+import discord
+from discord.ext import commands
+
+class Snowball(commands.Cog):
+
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command()
+    async def snowball(self, ctx):
+        await ctx.send(f"{random.choice(ctx.guild.members).mention}")
+
+async def setup(client):
+    await client.add_cog(Snowball(client))
