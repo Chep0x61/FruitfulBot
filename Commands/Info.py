29ff02eeb7fd4f7dfebfb782cd2f32610cbdf36f
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-19 10:41:00 +0100

    fix: typo in bot question

--- Commands/Info.py
+++ Commands/Info.py
@@ -20,7 +20,7 @@ class Info(commands.Cog):
         embed.add_field(name='Name:', value=member.display_name, inline=False),
         embed.add_field(name='Created at:', value=member.created_at, inline=False),
         embed.add_field(name='Joined at:', value=member.joined_at, inline=False),
-        embed.add_field(name='Are you a bot ?:', value=member.bot, inline=False),
+        embed.add_field(name='Are you a bot ?', value=member.bot, inline=False),
 
         await ctx.send(embed=embed)
 
